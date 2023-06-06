d87c1860e83e484bc51d84e0c7c0e63f2a6386db
Marco Pegoraro <marco.pegoraro@gmail.com>
2023-06-06 16:09:59 +0000

    Add demo sql

--- hasura-badges/sql/default/default.sql
+++ hasura-badges/sql/default/default.sql
@@ -1,1 +1,19 @@
-SELECT NOW();
+SELECT
+  bd.id,
+  bd.description,
+  COALESCE(
+    json_agg(
+      json_build_object(
+        'id', br.id,
+        'description', br.description
+      )
+    ),
+    '[]'::json
+  ) AS badge_reqs
+FROM
+  badge_defs bd
+LEFT JOIN
+  badge_reqs br ON bd.id = br.badge_defs_id
+GROUP BY
+  bd.id, bd.description;
+  
